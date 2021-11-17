e73175c3922e21528c54480d636da872e67e1105
gujo <gujo@itu.dk>
2021-11-17 17:22:06 +0100

    testing code coverage on SonarCloud

--- api.tests/Repository.Tests/TagRepositoryTests.cs
+++ api.tests/Repository.Tests/TagRepositoryTests.cs
@@ -9,15 +9,18 @@ namespace Repository.Tests
     public class TagRepositoryTests : TestDataGenerator
     {
         [Fact]
-        public void Given_something_returns_something()
+        public async void Given_something_returns_something()
         {
             // initiate the relevant repo for each unit test
-            // var _repo = new TagRepository(_context);
+            var _repo = new TagRepository(_context);
+            Seed(_context);
+            
+            var tag = await _repo.GetTagByIdAsync(1);
 
+            Assert.Equal("dotnet", tag.Name);
             // optionally seed the in-memory sqlite database with some dummy data
             // see the Seed method in TestDataGenerator
             //      - in the seed method you can put any kind of data you want to test with 
-            // Seed(_context);
 
             
             //Given
