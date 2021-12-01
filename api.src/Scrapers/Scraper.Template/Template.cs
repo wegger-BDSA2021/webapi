c4a413ca82603013fa9d5686288e3eb4ccdf7e1f
gujo <gujo@itu.dk>
2021-12-01 10:31:06 +0100

    added template method for the scrapers

---
+++ api.src/Scrapers/Scraper.Template/Template.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace Resource.Builder
+{
+    abstract class Template
+    {
+        protected string url;
+
+        // instead of this, retrieve tags from tagRepository
+        protected static string[] QueryTerms { get; } =
+        {
+            "anglesharp", "docker",
+            "dotnet" , ".NET",
+            "web scraping", "syntax",
+            "xpath", "html",
+            "github", "htmlagilitypack",
+            "c#", "pascal", "DTO",
+            "design pattern", "stopwatch",
+            "linq", "c", "c++"
+        };
+
+        public async Task Parse(string content, ResourceProduct product)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(content);
+
+            url = product.Url;
+
+            product.TitleFromSource = this.GetTitle(doc);
+            product.TagsFoundInSource = this.GetTags(doc).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        protected string GetTitle(HtmlDocument _doc)
+        {
+            var title = _doc.DocumentNode.SelectSingleNode("//title");
+            return title.InnerText;
+        }
+
+        protected abstract Task<ICollection<string>> GetTags(HtmlDocument _doc);
+    }
+}
