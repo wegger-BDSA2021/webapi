9aeec49f2bae8af6d325a8aca05b40b34276a32f
gujo <gujo@itu.dk>
2021-12-06 15:45:32 +0100

    resource endpoint almost done - only missing update resource

--- api.src/Controllers/ResourceController.cs
+++ api.src/Controllers/ResourceController.cs
@@ -17,13 +17,6 @@ namespace api.src.Controllers
             _service = service;
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ResourceDetailsDTO>> ReadSingleResource(int id)
-        {
-            var result = await _service.ReadAsync(id);
-            return result.ToActionResult();
-        }
-
         [HttpPost]
         public async Task<ActionResult<ResourceDetailsDTO>> CreateResource(ResourceCreateDTOClient resource)
         {
@@ -38,6 +31,16 @@ namespace api.src.Controllers
             return result.ToActionResult();
         }
 
+        // TODO : update resource endpoint ...
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResourceDetailsDTO>> ReadSingleResource(int id)
+        {
+            var result = await _service.ReadAsync(id);
+            return result.ToActionResult();
+        }
+
         [HttpGet]
         public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllResources()
         {
@@ -46,7 +49,7 @@ namespace api.src.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ICollection<ResourceDTO>>> GetAllFromUser(int id)
+        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllFromUser(int id)
         {
             var result = await _service.GetAllResourcesFromUserAsync(id);
             return result.ToActionResult();
@@ -60,14 +63,46 @@ namespace api.src.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllWithRatingInRange([FromBody] int from, [FromBody] int to)
+        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllWithAverageRatingInRange([FromBody] int from, [FromBody] int to)
         {
             var result = await _service.GetAllResourcesWithinRangeAsync(from, to);
             return result.ToActionResult();
         }
 
-        
-    
+        [HttpGet]
+        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllWhereTitleContians([FromBody] string matcher)
+        {
+            var result = await _service.GetAllResourcesWhereTitleContainsAsync(matcher);
+            return result.ToActionResult();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllMarkedDeprecated()
+        {
+            var result = await _service.GetAllResourcesMarkedDeprecatedAsync();
+            return result.ToActionResult();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllArticles()
+        {
+            var result = await _service.GetAllArticleResourcesAsync();
+            return result.ToActionResult();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllVideos()
+        {
+            var result = await _service.GetAllVideoResourcesAsync();
+            return result.ToActionResult();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<ResourceDTO>>> ReadAllWithProvidedTags([FromBody] ICollection<string> tags)
+        {
+            var result = await _service.GetAllResourcesWithProvidedTags(tags);
+            return result.ToActionResult();
+        }
 
     }
 }
