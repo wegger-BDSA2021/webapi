69192919324510f606a1dfdc593ee68b79b049e6
gues <gues@itu.dk>
2021-12-13 22:50:55 +0100

    Started on comment controller

---
+++ api.tests/Controller.Tests/CommentControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using api.src.Controllers;
+using api.src.Services;
+using Data;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static Data.Response;
+
+namespace api.tests.Controller.Tests
+{
+    /*public class CommentControllerTest
+    {
+        [Fact]
+        public async Task Update_given_unknown_id_returns_NotFound()
+        {
+            //Arrange
+            var toCreate = new CommentUpdateDTO();
+            var created = new CommentDetailsDTO(4, "testUserId", 6, DateTime.Now, "Content of comment");
+            var repository = new Mock<ICommentRepository>();
+            repository.Setup(c => c.UpdateComment(toCreate)).ReturnsAsync(NotFound);
+            var controller = new CommentController(repository.Object);
+
+            //Act
+            var response = await controller.UpdateComment(toCreate);
+
+            //Assert
+            
+        }
+    }*/
+}
