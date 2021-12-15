f3be4368ce224931a30bd53fc32cbfec57a396b5
Gustav <gues@it.dk>
2021-12-15 14:11:50 +0100

    Started on ResourceController Tests

---
+++ api.tests/Controller.Tests/ResourceControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using api.src;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.tests.Controller.Tests
+{
+    [Xunit.Collection("Sequential")]
+    class ResourceControllerTests : TestFixture
+    {
+        public ResourceControllerTests(WebApplicationFactory<Startup> factory) : base(factory) { }
+        
+
+
+    }
+}
