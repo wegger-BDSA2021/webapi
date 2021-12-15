89ea46405ff34025177398db365093cf42c09403
Rasmus Thorbjørn Overgaard Pedersen <rped@itu.dk>
2021-12-15 10:58:06 +0100

    delete and mock

--- api.tests/Service.Tests/RatingServiceTest.cs
+++ api.tests/Service.Tests/RatingServiceTest.cs
@@ -4,6 +4,7 @@ using Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Repository.Tests;
+using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,32 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using static Data.Response;
+using Moq;
 
-namespace api.tests.Controller.Tests
+
+namespace api.tests.Service.Tests
 {
     public class RatingServiceTests
     {
         private readonly RatingService _ratingService;
+        private readonly Mock<IRatingRepository> _RatingRepoMock = new();
+
         public RatingServiceTests()
         {
-            _ratingService = new RatingService();
+            _ratingService = new RatingService(_RatingRepoMock.Object);
+        }
+        [Fact]
+        public async Task Delete_given_non_existing_returns_NotFound()
+        {
+            //Arrange
+            _RatingRepoMock.Setup(c => c.DeleteAsync(42)).ReturnsAsync(NotFound);
+
+            //Act
+            var response = await _ratingService.Delete(42);
+
+            //Assert
+            Assert.Equal(NotFound, response.Response);
+            Assert.Equal("No comment found with id 42", response.Message);
         }
     }
 }
