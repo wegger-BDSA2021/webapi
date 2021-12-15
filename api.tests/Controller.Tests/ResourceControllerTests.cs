d30cfbdf4b090ed2ced7ca56f81252dd25abe510
Gustav <gues@it.dk>
2021-12-15 14:23:21 +0100

    Added ResourceController Test Methods For Get

--- api.tests/Controller.Tests/ResourceControllerTests.cs
+++ api.tests/Controller.Tests/ResourceControllerTests.cs
@@ -3,17 +3,32 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace api.tests.Controller.Tests
 {
     [Xunit.Collection("Sequential")]
-    class ResourceControllerTests : TestFixture
+    public class ResourceControllerTests : TestFixture
     {
         public ResourceControllerTests(WebApplicationFactory<Startup> factory) : base(factory) { }
-        
 
+        [Fact]
+        public async void Get_returns_HttpStatusCode_OK()
+        {
+            var response = await Client.GetAsync("/api/Resource/ReadAll");
 
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async void GetById_returns_HttpStatusCode_OK()
+        {
+            var response = await Client.GetAsync("/api/Resource{1}");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
     }
 }
