84f99989807434b2136e4432e6fb2a26d4c86ed1
Gustav <gues@it.dk>
2021-12-15 15:46:14 +0100

    Might work

--- api.tests/Controller.Tests/UserControllerTests.cs
+++ api.tests/Controller.Tests/UserControllerTests.cs
@@ -1,13 +1,6 @@
 ﻿using api.src;
-using Data;
 using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace api.tests.Controller.Tests
@@ -28,7 +21,7 @@ namespace api.tests.Controller.Tests
         [Fact]
         public async void Delete_returns_HttpStatusCode_NoContent()
         {
-            var response = await Client.DeleteAsync("/api/User/Delete/testUserId");
+            var response = await Client.DeleteAsync("/api/User/Delete/secondUserId");
 
             Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
         }
