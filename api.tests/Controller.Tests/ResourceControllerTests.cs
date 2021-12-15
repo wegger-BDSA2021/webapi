52e9dc58ae3df247af9a16406d518a093ad09399
Rasmus Thorbjørn Overgaard Pedersen <rped@itu.dk>
2021-12-15 16:52:36 +0100

    Update ResourceControllerTests.cs

--- api.tests/Controller.Tests/ResourceControllerTests.cs
+++ api.tests/Controller.Tests/ResourceControllerTests.cs
@@ -1,11 +1,11 @@
-﻿using api.src;
-using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using api.src;
+using Data;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace api.tests.Controller.Tests
@@ -14,6 +14,143 @@ namespace api.tests.Controller.Tests
     public class ResourceControllerTests : TestFixture
     {
         public ResourceControllerTests(WebApplicationFactory<Startup> factory) : base(factory) { }
+        /*       [Fact]
+        public async void Get_returns_HttpStatusCode_OK()
+        {
+            var response = await Client.GetAsync("/api/Resource/ReadAllWithTags");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        */
+        [Fact]
+        public async void GetAllFromOfficialDocumentationAsync_returns_HttpStatusCode_OK()
+        {
+            var response = await Client.GetAsync("/api/Resource/ReadAllOfficialDocumentation");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Fact]
+        public async void GetAllVideoResourcesAsync_returns_HttpStatusCode_OK()
+        {
+            var response = await Client.GetAsync("/api/Resource/ReadAllVideos");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Fact]
+        public async void GetAllArticleResourcesAsync_returns_HttpStatusCode_OK()
+        {
+            var response = await Client.GetAsync("/api/Resource/ReadAllArticles");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Fact]
+        public async void GetAllResourcesMarkedDeprecatedAsync_returns_HttpStatusCode_OK()
+        {
+            var response = await Client.GetAsync("/api/Resource/ReadAllDeprecated");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async void ReadAllWhereTitleContians_returns_HttpStatusCode_OK()
+        {
+            var matcher = "test";
+
+            var response = await Client.GetAsync($"/api/Resource/ReadAllWithTitle/{matcher}");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAllResourcesWithinRangeAsync_returns_HttpStatusCode_OK()
+        {
+
+            var response = await Client.GetAsync("/api/Resource/ReadAllWithAverageRatingInRange/1/2");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAllResourcesFromDomainAsync_returns_HttpStatusCode_OK()
+        {     
+            var matcher = "test";
+
+            var response = await Client.GetAsync($"/api/Resource/ReadAllFromDomain/{matcher}");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Fact]
+        public async void GetAllResourcesFromUserAsync_returns_HttpStatusCode_OK()
+        {     
+            var id = "testUserId";
+
+            var response = await Client.GetAsync($"/api/Resource/ReadAllFromUser/{id}");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Fact]
+        public async void GetAllResourcesFromUserAsync_returns_HttpStatusCode_BadRequest()
+        {     
+            var id = "";
+
+            var response = await Client.GetAsync($"/api/Resource/ReadAllFromUser/{id}");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+        [Fact]
+        public async void GetAllResourcesFromUserAsync_returns_HttpStatusCode_NotFound()
+        {     
+            var id = "IDontExist";
+
+            var response = await Client.GetAsync($"/api/Resource/ReadAllFromUser/{id}");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+        [Fact]
+        public async void ReadSingleResource_returns_HttpStatusCode_OK()
+        {     
+            var response = await Client.GetAsync("/api/Resource/1");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        
+        [Fact]
+        public async void GetById_given_negative_returns_HttpStatusCode_BadRequest()
+        {
+            var Resourceid = -69;
+
+            var response = await Client.GetAsync($"/api/Resource/{Resourceid}");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+        [Fact]
+        public async void ReadSingleResource_returns_HttpStatusCode_NotFound()
+        {     
+            var response = await Client.GetAsync("/api/Resource{69}");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async void ReadAll_returns_HttpStatusCode_OK()
+        {     
+            var response = await Client.GetAsync($"/api/Resource/ReadAll");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+        [Fact]
+        public async void UpdateResource_returns_HttpStatusCode_OK()
+        {
+            var resource = new ResourceUpdateDTO
+            {
+                Id = 27,
+                Deprecated = false,
+            };
+                 
+            var response = await Client.GetAsync($"/api/Resource/Update");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
 
         [Fact]
         public async void Get_returns_HttpStatusCode_OK()
@@ -24,11 +161,24 @@ namespace api.tests.Controller.Tests
         }
 
         [Fact]
-        public async void GetById_returns_HttpStatusCode_NotFound()
+        public async void GetById_returns_HttpStatusCode_OK()
         {
-            var response = await Client.GetAsync("/api/Resource{1}");
+            var Resourceid = 1;
+            var response = await Client.GetAsync($"/api/Resource{Resourceid}");
 
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
+        /*
+        [Fact]
+        public async void GetById_given_negative_returns_HttpStatusCode_BadRequest()
+        {
+            var Resourceid = 0;
+            Resourceid -= 5;
+
+            var response = await Client.GetAsync($"/api/Resource{Resourceid}");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }*/
+        
     }
 }
