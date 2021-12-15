009aeac75419bcde4d7a3b3ea9279d813e430aae
Gustav <gues@it.dk>
2021-12-15 16:19:42 +0100

    Added tests again

--- api.tests/Controller.Tests/RatingControllerTests.cs
+++ api.tests/Controller.Tests/RatingControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿using api.src;
+using Data;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -16,7 +18,6 @@ namespace api.tests.Controller.Tests
         public RatingControllerTests(WebApplicationFactory<Startup> factory) : base(factory) { }
 
         [Fact]
-
         public async void Get_Ratings_From_Resource_returns_HttpStatusCode_OK()
         {
             var response = await Client.GetAsync("/api/Rating/Resource/1");
@@ -25,13 +26,52 @@ namespace api.tests.Controller.Tests
         }
 
         [Fact]
+        public async void Post_returns_HttpStatusCode_Created()
+        {
+            //Arrange
+            var rating = new RatingCreateDTO
+            {
+                UserId = "testUserId",
+                ResourceId = 1,
+                Rated = 2
+            };
+
+            JsonContent content = JsonContent.Create(rating);
+
+            //Act
+            var response = await Client.PostAsync("/api/Rating", content);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async void Update_returns_HttpStatusCode_NoContent()
+        {
+            //Arrange
+            var rating = new RatingCreateDTO
+            {
+                UserId = "testUserId",
+                ResourceId = 1,
+                Rated = 3
+            };
+
+            JsonContent content = JsonContent.Create(rating);
+
+            //Act
+            var response = await Client.PutAsync("/api/Rating", content);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+
+        [Fact]
         public async void GetById_returns_HttpStatusCode_OK()
         {
-            var response = await Client.GetAsync("/api/Comment/1");
+            var response = await Client.GetAsync("/api/Rating/1");
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
-
 
         [Fact]
         public async void Delete_returns_HttpStatusCode_NoContent()
